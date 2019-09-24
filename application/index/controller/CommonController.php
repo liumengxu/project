62fc591fe0621a61e289b1ff4ebfd248c7920552
刘梦雪 <liumengxue@tapai.tv>
2019-09-24 14:22:29 +0800

    aa

---
+++ application/index/controller/CommonController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\index\controller;
+
+
+class CommonController extends Controller
+{
+    public function a(){
+        echo 2222;
+    }
+}
---
+++ application/index/controller/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\index\controller;
+
+
+use think\App\model;
+
+class userController extends CommonController
+{
+
+}
